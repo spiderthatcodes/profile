a30802391ed6efdadfbb7ab90631fe1fe886fd47
jdickerson-banyan <75698869+jdickerson-banyan@users.noreply.github.com>
2022-02-25 20:35:15 -0700

    more edits, added parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,59 @@
 import React from 'react'
-import Header from './Components/Header/Header'
-import CssBaseline from '@mui/material/CssBaseline'
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
+import { Parallax } from 'react-materialize'
+
+const bark = require('./images/bark.jpg')
 
 function App () {
   return (
     <>
-      <CssBaseline />
-      <Container maxWidth='xl'>
-        <Box sx={{ bgcolor: '#D3D3D3', height: '100vh', padding: '10px'}} >
-          <Header />
-        </Box>
-      </Container>
+      <div>
+      <div className='section white'>
+          <div className='row container'>
+            <h1 className='header'>Jessica Dickerson</h1>
+            <p className='grey-text text-darken-3 lighten-3'>
+              Software Developer and Certified Professional Scrum Master
+            </p>
+          </div>
+        </div>
+        <Parallax
+          image={<img alt='' src={bark} />}
+          options={{
+            responsiveThreshold: 0
+          }}
+        />
+        <div className='section white'>
+          <div className='row container'>
+            <h2 className='header'></h2>
+            <p className='grey-text text-darken-3 lighten-3'>
+              Parallax is an effect where the background content or image in
+              this case, is moved at a different speed than the foreground
+              content while scrolling.
+            </p>
+          </div>
+        </div>
+        <Parallax
+          image={<img alt='' src={bark} />}
+          options={{
+            responsiveThreshold: 0
+          }}
+        />
+        <div className='section white'>
+          <div className='row container'>
+            <h2 className='header'>Parallax</h2>
+            <p className='grey-text text-darken-3 lighten-3'>
+              Parallax is an effect where the background content or image in
+              this case, is moved at a different speed than the foreground
+              content while scrolling.
+            </p>
+          </div>
+        </div>
+        <Parallax
+          image={<img alt='' src={bark} />}
+          options={{
+            responsiveThreshold: 0
+          }}
+        />
+      </div>
     </>
   )
 }
